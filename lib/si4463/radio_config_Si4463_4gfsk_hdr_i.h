52cb00662639fb4cbae375b472215b16b468884d
Michael K <mkarpov2@illinois.edu>
2026-02-27 01:40:20 -0600

    asdfasdf

--- lib/si4463/radio_config_Si4463_4gfsk_hdr_i.h
+++ lib/si4463/radio_config_Si4463_4gfsk_hdr_i.h
@@ -52,7 +52,7 @@
 // Command:                  RF_GPIO_PIN_CFG
 // Description:              Configures the GPIO pins.
 */
-#define RF_GPIO_PIN_CFG 0x13, 0x21, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00
+#define RF_GPIO_PIN_CFG 0x13, 0x21, 0x08, 0x21, 0x20, 0x00, 0x00, 0x00
 
 /*
 // Set properties:           RF_GLOBAL_XO_TUNE_2
