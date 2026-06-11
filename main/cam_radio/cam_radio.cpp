d832c8596486df766b2dec33b32cea029b843748
Eddie <eddietang2314@gmail.com>
2026-06-10 20:58:11 -0700

    Move fsk code to radio driver now.

--- main/cam_radio/cam_radio.cpp
+++ main/cam_radio/cam_radio.cpp
@@ -2,72 +2,12 @@
 
 CAMRadioStatus CAMRadio::init(SPIClass &spi)
 {
-    //     Si4463Pins pins = {
-    //         .spi   = &spi,
-    //         .cs    = SI4463_CS,
-    //         .sdn   = SI4463_SDN,
-    //         .irq   = SI4463_INT,
-    // #ifdef SI4463_GPIO0
-    //         .gpio0 = SI4463_GPIO0,
-    // #else
-    //         .gpio0 = 0xFF,
-    // #endif
-    // #ifdef SI4463_GPIO1
-    //         .gpio1 = SI4463_GPIO1,
-    // #else
-    //         .gpio1 = 0xFF,
-    // #endif
-    //     };
-
-    //     if (!_radio.begin(pins)) {
-    //         Serial.printf("[CAMRadio] init failed, err=%u\n", _radio.getLastError());
-    //         return CAMRADIO_INIT_ERR;
-    //     }
-
-    //     // Print config for debug verification
-    //     _radio.printConfig(Serial);
-    //     _radio.printDebug(Serial);
-
-    //     Serial.println("[CAMRadio] init OK (Si4463Nuke driver)");
-    return CAMRADIO_OK;
-}
-
-CAMRadioStatus CAMRadio::send(const uint8_t *data, uint32_t len)
-{
-    if (!_radio.tx(data, len))
+    LR2021Error err = _radio.init(spi); // not _radio.begin(pins)
+    if (err.driverCode != LR2021_ERR_NONE)
     {
-        Serial.printf("[CAMRadio] tx failed, err=%u\n", _radio.getLastError());
+        Serial.printf("[CAMRadio] init failed, err=%d\n", err.driverCode);
         return CAMRADIO_INIT_ERR;
     }
+    Serial.println("[CAMRadio] init OK (LR2021 FSK driver)");
     return CAMRADIO_OK;
-}
-
-void CAMRadio::startRx(uint8_t *buf, uint32_t bufLen)
-{
-    _radio.startRx(buf, bufLen);
-}
-
-void CAMRadio::update()
-{
-    _radio.update();
-}
-
-bool CAMRadio::isTxBusy() const
-{
-    return _radio.isBusy();
-}
-
-bool CAMRadio::available()
-{
-    return _radio.available();
-}
-
-uint32_t CAMRadio::getReceivedLength() const
-{
-    return _radio.getReceivedLength();
-}
-
-int CAMRadio::getRSSI() const
-{
-    return _radio.getRSSI();
 }
--- main/cam_radio/cam_radio.h
+++ main/cam_radio/cam_radio.h
@@ -1,11 +1,11 @@
 #pragma once
-#include <Si4463Nuke.h>
-#include <pins.h>
-#include <SPI.h>
 
 #include "fsk.h"
 #include "fsk_framer.h"
 #include "fsk_reassembler.h"
+
+#include <pins.h>
+#include <SPI.h>
 
 enum CAMRadioStatus
 {
@@ -16,34 +16,12 @@ enum CAMRadioStatus
 class CAMRadio
 {
 private:
-    Si4463Nuke _radio;
+    LR2021FSKDriver _radio;
 
 public:
     // Initialize radio hardware. Call after SPI.begin().
     CAMRadioStatus init(SPIClass &spi);
 
-    // Transmit arbitrary-length data (>50KB supported, auto-fragmented).
-    // Buffer must stay valid until isTxBusy() returns false.
-    CAMRadioStatus send(const uint8_t *data, uint32_t len);
-
-    // Start receiving into provided buffer.
-    void startRx(uint8_t *buf, uint32_t bufLen);
-
-    // Poll radio state machine. Call every loop iteration during TX or RX.
-    void update();
-
-    // True while a transmission is in progress.
-    bool isTxBusy() const;
-
-    // True once after a complete message has been received.
-    bool available();
-
-    // Length of last received message.
-    uint32_t getReceivedLength() const;
-
-    // RSSI of last received packet (dBm).
-    int getRSSI() const;
-
-    // Access underlying driver for debug/advanced use.
-    Si4463Nuke *getDriver() { return &_radio; }
+    // // Access underlying driver for debug/advanced use.
+    LR2021FSKDriver *getDriver() { return &_radio; }
 };
